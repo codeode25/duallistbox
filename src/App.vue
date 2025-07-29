05756ae03eb4fd6be56d7bd4b220cc717b3f5f85
poxicatalin <pocsanjr@proton.me>
2025-07-29 09:34:22 +0300

    bring in dual list box component and imagine api for it

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,59 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import DualListBox from './components/DualListBox.vue';
+
+const groupData = ref([
+  {
+    group: 'Group 1',
+    items: [
+      {
+        label: 'Item 1',
+        value: 1,
+      },
+      {
+        label: 'Item 2',
+        value: 2,
+      },
+      {
+        label: 'Item 3',
+        value: 3,
+      }
+    ],
+  }
+])
+
+const flatData = ref([
+  {
+    label: 'Item 1',
+    value: 1,
+  },
+  {
+    label: 'Item 2',
+    value: 2,
+  },
+  {
+    label: 'Item 3',
+    value: 3,
+  }
+])
+
+
+const destination = ref([]);
+
+const transfer = (source: any[], destination: any[]) => {
+  console.log(source, destination);
+}
+
 
 </script>
 
 <template>
-  hello
+  <DualListBox 
+    :source="groupData"
+    :destination="destination"
+    :groupMode="true"
+    @transfer="transfer"
+  />
 </template>
 
 <style scoped>
