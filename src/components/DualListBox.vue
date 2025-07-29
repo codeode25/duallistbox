bc822d2e94b282a9cb1b5aac793ba8aa16915b17
poxicatalin <pocsanjr@proton.me>
2025-07-29 09:43:21 +0300

    define props emit for dual list box component

--- src/components/DualListBox.vue
+++ src/components/DualListBox.vue
@@ -1,4 +1,24 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+
+const props = defineProps(['source', 'destination', 'groupMode']);
+const emit = defineEmits(['transfer']);
+
+const source = ref(initItems(props.source, props.groupMode));
+const destination = ref(initItems(props.destination, props.groupMode));
+
+function initItems(items: any[], groupMode: boolean) {
+    return items.map((item) => {
+        if (groupMode) {
+            item.items = item.items.map((it) => ({...it, selected: false}));
+        } else {
+            item = {...item, selected: false};
+        }
+
+        return item;
+    })
+}
 
 </script>
 
